c97ca5bd5e3699f6eca3ddc9c3090d5fe49a8f5b
tommyHumble <49183491+tommyHumble@users.noreply.github.com>
2019-04-03 10:10:58 +0300

    Delete UDP_Frame_Server.py

--- UDP_Frame_Server.py
+++
@@ -1,7 +0,0 @@
-import socket
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-for i in range(10):
-    sock.sendto(bytes('IT\'S UDP REQUEST FROM {}, I NEED A RESPOND ABOUT PORT {}'.format('192.168.226.115', 8880 + i ), encoding='utf-8'), ("192.168.226.115", 8880 + i))
-    print(sock.recv(1024).decode())
